d597f67f7ff33fa1199e17d39e98775112d55776
KAUA MARTINS DE ANDRADE <202502971532@alunos.unifanor.edu.br>
2025-09-10 20:51:11 -0300

    atualizando

--- LISTAS/lista.py
+++ LISTAS/lista.py
@@ -5,5 +5,16 @@ frutas = ['Caju', 'Kiwi', 'Maçã', 'Uva']
 # print(frutas)
 
 # Exibir elementos usando o for
-for fruta in frutas:
-    print(fruta)
+# for fruta in frutas:
+#     print(fruta)
+
+# Exibir elemento especifico da lista
+# print(frutas[-1])
+
+# COMO ADICIONAR UM ELEMENTO A LISTA?
+# Use a função append
+
+# frutas.append('Acerola')
+# print(frutas)
+
+print(len(frutas))
